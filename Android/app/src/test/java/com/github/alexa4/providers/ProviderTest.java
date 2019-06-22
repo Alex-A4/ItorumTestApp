d6a9a40afea3fa756dabc169a982e7bba0b3783f
Alex-A4 <alex.a4.25@yandex.ru>
2019-06-22 12:46:22 +0300

    Add more tests

--- Android/app/src/test/java/com/github/alexa4/providers/ProviderTest.java
+++ Android/app/src/test/java/com/github/alexa4/providers/ProviderTest.java
@@ -3,6 +3,7 @@ package com.github.alexa4.providers;
 import com.github.alexa4.controllers.Downloader;
 import com.github.alexa4.models.People;
 
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,9 +11,11 @@ import static org.junit.Assert.*;
 
 public class ProviderTest {
     Provider<People> mPeople = new Provider<>(People.class);
+
     @Before
     public void setUp() throws Exception {
-        new Downloader<People>().downloadPage("https://swapi.co/api/people/?page=1", mPeople);
+        new Downloader<People>()
+                .downloadPage("https://swapi.co/api/people/?page=1", mPeople);
     }
 
     @Test
@@ -31,5 +34,27 @@ public class ProviderTest {
     public void getPeopleByIndexUpperSize() {
         People people = mPeople.getItemByIndex(15);
         assertNull(people);
+    }
+
+    @Test
+    public void testParsingTrue() throws Exception {
+        Provider<People> people = new Provider<>(People.class);
+        String data = new Downloader<People>()
+                .makeCall("https://swapi.co/api/people/?page=1");
+
+        JSONObject json = new JSONObject(data);
+        people.parseData(json);
+        assertTrue(people.isLoaded());
+    }
+
+    @Test
+    public void testParsingFalse() throws Exception {
+        Provider<People> people = new Provider<>(People.class);
+        String data = new Downloader<People>()
+                .makeCall("https://swapi.co/api/planets/?page=1");
+
+        JSONObject json = new JSONObject(data);
+        people.parseData(json);
+        assertFalse(people.isLoaded());
     }
 }
